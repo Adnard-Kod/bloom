4f19ada16c83b088bd590868c1c6cd4c192e2e5c
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 15:50:00 -0700

    Create test to ensure user cannot be created with invalid parameters.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe UsersController, :type => :controller do
         post :create, user: FactoryGirl.attributes_for(:user)
       }.to change{ User.count }.by(1)
     end
+
+    it "does not create a new user with invalid parameters" do
+      expect {
+        post :create, user: { email: '' }
+      }.to_not change{ User.count }
+    end
   end
 end
