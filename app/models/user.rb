236b8aa4092ba70abd68ca4a1024ea78c8d41abf
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-15 18:27:51 -0700

    Add bcrypt to Gemfile. Run bundle install to update dependencies in Gemfile.lock. Add has_secure_password to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   validates :email, :first_name, :last_name, :phone_number, :password_digest, presence: true
   validates :email, uniqueness: true
   has_many :addresses
