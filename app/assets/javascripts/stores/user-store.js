478c3234224c74b720c64c2b5f1c3f0533da5162
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 14:59:52 -0700

    Update UserStore to redirect user to root_path upon successful user creation. On user creation failure UserStore triggers 'creation-error' and passes error messages to UserCreation component.

--- app/assets/javascripts/stores/user-store.js
+++ app/assets/javascripts/stores/user-store.js
@@ -1,3 +1,20 @@
-var UserStore = (function () {
+//= require dispatcher
 
+var UserStore = (function () {
+  return {
+    create: function (userData) {
+
+      $.ajax({
+        type: 'POST',
+        url: '/users',
+        data: userData
+      })
+      .done(function(data) {
+        window.location = data.redirect;
+      })
+      .fail(function (xhr, status, selse) {
+        $(this).trigger('creation-error', JSON.parse(xhr.responseText));
+      }.bind(this));
+    }
+  }
 }());
