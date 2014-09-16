fd2948a2d0f90018409f3cd4fa50fb67aab1cb87
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 16:27:21 -0700

    Add remaining blank parameters tests for user creation. [Finishes #78846736]

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -26,8 +26,28 @@ RSpec.describe UsersController, :type => :controller do
         post :create, user: { email: '', first_name: '', last_name: '', phone_number: '', password: '', password_confirmation: ''}
       end
 
-      it "returns an error message when creating a user with invalid password" do
+      it "returns an error message when creating a user with blank password" do
         expect(JSON.parse(response.body)['errors'][0]).to include("Password can't be blank")
+      end
+
+      it "returns an error message when creating a user with blank email" do
+        expect(JSON.parse(response.body)['errors'][1]).to include("Email can't be blank")
+      end
+
+      it "returns an error message when creating a user with blank first name" do
+        expect(JSON.parse(response.body)['errors'][2]).to include("First name can't be blank")
+      end
+
+      it "returns an error message when creating a user with blank last name" do
+        expect(JSON.parse(response.body)['errors'][3]).to include("Last name can't be blank")
+      end
+
+      it "returns an error message when creating a user with blank phone number" do
+        expect(JSON.parse(response.body)['errors'][4]).to include("Phone number can't be blank")
+      end
+
+      it "returns an error message when creating a user with blank password confirmation" do
+        expect(JSON.parse(response.body)['errors'][5]).to include("Password confirmation doesn't match Password")
       end
     end
   end
