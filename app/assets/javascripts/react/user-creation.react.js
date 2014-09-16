92244e5222eb3395b58a969dfa47a06a24a461fc
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 15:07:13 -0700

    Update UserCreation component.  - Require form-error  - Set initial error state.  - Set listener, upon component mounting, for error state change.  - Render errors upon user creation failure.

--- app/assets/javascripts/react/user-creation.react.js
+++ app/assets/javascripts/react/user-creation.react.js
@@ -1,11 +1,31 @@
 /** @jsx React.DOM */
 //= require react
 //= require stores/user-store
+//= require react/form-error.react
 
 var UserCreation = React.createClass({displayName: 'UserCreation',
+  getInitialState: function () {
+    return {
+      errors: []
+    }
+  },
+
+  componentDidMount: function () {
+    $(UserStore).on('creation-error', function (e, userCreationErrors) {
+      this.setState({
+        errors: userCreationErrors.errors
+      })
+    }.bind(this));
+  },
+
   render: function () {
+    var errors = [];
+    this.state.errors.forEach(function(err) {
+      errors.push(FormError({error: err}))
+    })
     return (
       React.DOM.section({id: "user-signup-box"}, 
+        React.DOM.ul({className: "errors"}, errors), 
         React.DOM.p({id: "user-signup-title"}, "Create An Account"), 
         React.DOM.form({id: "user-form", onSubmit: this.createUser}, 
           React.DOM.input({ref: "first_name", placeholder: "First Name"}), 
--- app/assets/react/user-creation.react.js
+++ app/assets/react/user-creation.react.js
@@ -1,11 +1,31 @@
 /** @jsx React.DOM */
 //= require react
 //= require stores/user-store
+//= require react/form-error.react
 
 var UserCreation = React.createClass({
+  getInitialState: function () {
+    return {
+      errors: []
+    }
+  },
+
+  componentDidMount: function () {
+    $(UserStore).on('creation-error', function (e, userCreationErrors) {
+      this.setState({
+        errors: userCreationErrors.errors
+      })
+    }.bind(this));
+  },
+
   render: function () {
+    var errors = [];
+    this.state.errors.forEach(function(err) {
+      errors.push(<FormError error = {err}/>)
+    })
     return (
       <section id="user-signup-box">
+        <ul className="errors">{errors}</ul>
         <p id="user-signup-title">Create An Account</p>
         <form id="user-form" onSubmit={this.createUser}>
           <input ref="first_name" placeholder="First Name" />
