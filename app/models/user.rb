976eb09b27f1d48bd044e9c9cc2511cd7476d8dd
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-15 18:43:21 -0700

    Update password validation in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, :first_name, :last_name, :phone_number, :password_digest, presence: true
+  validates :email, :first_name, :last_name, :phone_number, presence: true
   validates :email, uniqueness: true
+  validates_confirmation_of :password
   has_many :addresses
 end
