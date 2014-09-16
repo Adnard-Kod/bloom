b545676aea35a101004697f3b90522c992718ad8
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 15:41:28 -0700

    Create test to verify successful creation of a new user via create action.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+  let(:user) { create(:user) }
+
   describe "POST #create" do
-    it "creates a new user with valid parameters"
+    it "creates a new user with valid parameters" do
+      expect {
+        post :create, user: FactoryGirl.attributes_for(:user)
+      }.to change{ User.count }.by(1)
+    end
   end
 end
