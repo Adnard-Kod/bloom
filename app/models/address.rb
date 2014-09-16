0a0838243272faa86924b8c525c18722e4a42f09
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-15 18:19:36 -0700

    Add basic validations to address and user models.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ActiveRecord::Base
+  validates :street_address, :city, :state, :zipcode, :user_id, presence: true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  validates :email, :first_name, :last_name, :phone_number, :password_digest, presence: true
+  validates :email, uniqueness: true
   has_many :addresses
 end
