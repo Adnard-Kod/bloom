9df2bbe187ebaa48d068a1c8cf27d51d36f7524b
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 17:50:45 -0700

    Created user-login initial rendering. Added userLogin method to UserLogin component.

--- app/assets/javascripts/react/user-login.react.js
+++ app/assets/javascripts/react/user-login.react.js
@@ -2,5 +2,26 @@
 //= require react
 
 var UserLogin = React.createClass({displayName: 'UserLogin',
+  render: function () {
+    return (
+      React.DOM.nav({id: "user-login"}, 
+        React.DOM.p(null, "Login"), 
+        React.DOM.form({id: "user-login-form", onSubmit: "this.userLogin"}, 
+          React.DOM.input({ref: "email", placeholder: "example@example.com", type: "email"}), 
+          React.DOM.input({ref: "password", placeholder: "password", type: "password", maxLength: "30"}), 
+          React.DOM.input({type: "submit", value: "Login"})
+        )
+      )
+    )
+  },
 
+  userLogin: function (e) {
+    e.preventDefault();
+    var formData = {};
+    formData.user = {};
+    Object.keys(this.refs).forEach(function (ref) {
+      formData.user[ref] = this.refs[ref].getDOMNode().value;
+    }.bind(this));
+    SessionsStore.login(formData);
+  }
 });
--- app/assets/react/user-login.react.js
+++ app/assets/react/user-login.react.js
@@ -2,5 +2,26 @@
 //= require react
 
 var UserLogin = React.createClass({
+  render: function () {
+    return (
+      <nav id="user-login">
+        <p>Login</p>
+        <form id="user-login-form" onSubmit="this.userLogin">
+          <input ref="email" placeholder="example@example.com" type="email" />
+          <input ref="password" placeholder="password" type="password" maxLength="30" />
+          <input type="submit" value="Login" />
+        </form>
+      </nav>
+    )
+  },
 
+  userLogin: function (e) {
+    e.preventDefault();
+    var formData = {};
+    formData.user = {};
+    Object.keys(this.refs).forEach(function (ref) {
+      formData.user[ref] = this.refs[ref].getDOMNode().value;
+    }.bind(this));
+    SessionsStore.login(formData);
+  }
 });
