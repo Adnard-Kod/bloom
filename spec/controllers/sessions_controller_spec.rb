26da64ebed0b3d9c25dbf3b0ad080c7d3dbd8d89
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-17 16:48:02 -0700

    Create before to create posts prior to each sessions create test. Add test to verify proper redirect path is sent back via JSON.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,9 +4,16 @@ RSpec.describe SessionsController, :type => :controller do
   let(:user) { create(:user) }
 
   describe "POST #create" do
-    it "should create a session for a valid user" do
+    before :each do
       post :create, user: { email: user.email , password: user.password }
+    end
+
+    it "should create a session for a valid user" do
       expect(session[:user_id]).to eq(user.id)
+    end
+
+    it "redirects to user dashboard upon successful login" do
+      expect(JSON.parse(response.body)["redirect"]).to eq(user_dashboard_index_path)
     end
   end
 end
