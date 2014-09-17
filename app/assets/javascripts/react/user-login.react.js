460ffd294a4b3bf1038aba2cc29badfe625e3257
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 18:59:53 -0700

    Add getInitialState and componentDidMount for user login error handling. Update render function.

--- app/assets/javascripts/react/user-login.react.js
+++ app/assets/javascripts/react/user-login.react.js
@@ -1,11 +1,32 @@
 /** @jsx React.DOM */
 //= require react
 //= require stores/session-store
+//= require react/form-error.react
 
 var UserLogin = React.createClass({displayName: 'UserLogin',
+  getInitialState: function() {
+    return {
+      errors: []
+    };
+  },
+
+  componentDidMount: function() {
+    $(SessionStore).on('login-error', function (e, userLoginErrors) {
+      this.setState({
+        errors: userLoginErrors.errors
+      })
+    }.bind(this));
+  },
+
   render: function () {
+    var errors = [];
+    this.state.errors.forEach(function (err) {
+      errors.push(FormError({error: err}))
+    });
+
     return (
       React.DOM.nav({id: "user-login"}, 
+        React.DOM.ul({className: "errors"}, errors), 
         React.DOM.p(null, "Login"), 
         React.DOM.form({id: "user-login-form", onSubmit: this.userLogin}, 
           React.DOM.input({ref: "email", placeholder: "example@example.com", type: "email"}), 
--- app/assets/react/user-login.react.js
+++ app/assets/react/user-login.react.js
@@ -1,11 +1,32 @@
 /** @jsx React.DOM */
 //= require react
 //= require stores/session-store
+//= require react/form-error.react
 
 var UserLogin = React.createClass({
+  getInitialState: function() {
+    return {
+      errors: []
+    };
+  },
+
+  componentDidMount: function() {
+    $(SessionStore).on('login-error', function (e, userLoginErrors) {
+      this.setState({
+        errors: userLoginErrors.errors
+      })
+    }.bind(this));
+  },
+
   render: function () {
+    var errors = [];
+    this.state.errors.forEach(function (err) {
+      errors.push(<FormError error = {err} />)
+    });
+
     return (
       <nav id="user-login">
+        <ul className="errors">{errors}</ul>
         <p>Login</p>
         <form id="user-login-form" onSubmit={this.userLogin}>
           <input ref="email" placeholder="example@example.com" type="email" />
