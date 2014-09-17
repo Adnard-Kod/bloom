d947ddc725cab60d51f11076ecff20b5aab6e095
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-17 16:25:14 -0700

    Add user/dashboard namespaced route.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resources :subscriptions, :only => [:index]
   resources :users, except: [:new, :edit]
   resource :sessions, only: [:create, :destroy]
+
+  namespace :user do
+    resources :dashboard, only: [:index]
+  end
 end
