c84ed494ba4107a69ec54b9c01297e0936beddf6
Gunnari Auvinen & Ernie Salazar <pair+cheerazar.ehsalazar@devbootcamp.com>
2014-09-16 18:17:48 -0700

    Create session upon successful user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      session[:user_id] = user.id
       render json: { redirect: root_path }
     else
       render json: { success: false, :errors => user.errors.full_messages }, status: :unprocessable_entity
