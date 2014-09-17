3465c3ed73923a1c7fe19aea64f51159ae4a000c
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-17 14:27:29 -0700

    Add test to verify a session properly being set for a user.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
+  let(:user) { create(:user) }
+
+  describe "POST #create" do
+    it "should create a session for a valid user" do
+      post :create, user: { email: user.email , password: user.password }
+      expect(session[:user_id]).to eq(user.id)
+    end
+  end
 end
