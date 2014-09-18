382606a5fe1ecd0739c281e82cdd08ade1cfe41c
Steven Leu <steven.c.leu@gmail.com>
2014-09-17 21:42:12 -0700

    Add admin/menus#create route

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -2,4 +2,12 @@ class Admin::MenusController < AdminController
   def index
     render json: Menu.all
   end
+
+  def create
+    menu = Menu.new(menu_params)
+    if menu.save
+      render json: menu
+    else
+      render json: {errors: menu.errors.full_messages}, status: :unprocessable_entity
+    end
 end
