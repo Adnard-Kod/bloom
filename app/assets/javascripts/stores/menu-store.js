0978d9256edb9a43d62cb8e17a5999193d281277
Steven Leu <steven.c.leu@gmail.com>
2014-09-17 22:06:54 -0700

    Add create, edit, and delete ajax functions to the menu store

--- app/assets/javascripts/stores/menu-store.js
+++ app/assets/javascripts/stores/menu-store.js
@@ -1,7 +1,7 @@
 var MenuStore = (function() {
   var _menus = [];
   var CHANGE_EVENT = 'change';
-
+  var FAIL_TO_CREATE_EVENT = 'creation-failed';
   return {
     menus: function() {
       return _menus;
@@ -19,8 +19,73 @@ var MenuStore = (function() {
     addChangeEvent: function(callback) {
       $(this).on(CHANGE_EVENT, callback);
     },
+    removeChangeEvent: function(obj) {
+      $(this).off(CHANGE_EVENT, obj);
+    },
+    addFailToTakeAction: function(callback) {
+      $(this).on(FAIL_TO_CREATE_EVENT, callback);
+    },
+    removeFailToTakeAction: function(obj) {
+      $(this).off(FAIL_TO_CREATE_EVENT, obj);
+    },
+    triggerFailToTakeAction: function(data) {
+      $(this).trigger(FAIL_TO_CREATE_EVENT, data);
+    },
     triggerChange: function(data) {
       $(this).trigger(CHANGE_EVENT, data);
+    },
+    submit: function(data) {
+      data.editing ? this.edit(data.menu) : this.create(data.menu);
+    },
+    create: function(menu) {
+      $.ajax({
+        url: '/admin/menus',
+        type: 'POST',
+        data: {menu: menu}
+      })
+      .done(function(data) {
+        _menus.push(data.menu)
+        this.triggerChange();
+      }.bind(this))
+      .fail(function(xhr) {
+        this.triggerFailToTakeAction([xhr.responseJSON.errors]);
+      }.bind(this))
+    },
+    edit: function(menu) {
+      $.ajax({
+        url: '/admin/menus' + menu.id,
+        type: 'PUT',
+        data: {menu: menu}
+      })
+      .done(function(data) {
+        _menus.forEach(function(menu, i) {
+          if(menu.id === data.menu.id) {
+            _menus[i] = data.menu;
+            return this.triggerChange();
+          }
+        }.bind(this))
+      }.bind(this))
+      .fail(function(xhr) {
+        this.triggerFailToTakeAction([xhr.responseJSON.errors]);
+      }.bind(this))
+    },
+    destroy: function(id) {
+      $.ajax({
+        url: '/admin/menus/'+id,
+        type: 'DELETE',
+        data: {id: id}
+      })
+      .done(function(data) {
+        _menus.forEach(function(menu, i) {
+          if (menu.id === data.id) {
+            _menus.splice(i, 1);
+            return this.triggerChange();
+          }
+        }.bind(this))
+      }.bind(this))
+      .fail(function(xhr) {
+        this.triggerFailToTakeAction([xhr.responseJSON.errors]);
+      }.bind(this))
     }
   }
 }())
