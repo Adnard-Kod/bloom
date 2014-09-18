a5d126e16118fa96c39bfa3d91477c5d5656424a
Steven Leu & Ernie Salazar <pair+eksentrysyti.ehsalazar@devbootcamp.com>
2014-09-18 16:39:45 -0700

    Add admin.css for admin layout assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( application.js admin.js user.js user.css)
+Rails.application.config.assets.precompile += %w( application.js admin.js user.js application.js admin.css user.css)
