ec7b6ed31ed3dbb0a4bb5706e773de5513fccab5
Ed Shadi <edshadi@gmail.com>
2014-09-20 17:14:51 -0700

    clean up getting input value from form refs

--- app/assets/react/form-builder/form-for.react.js
+++ app/assets/react/form-builder/form-for.react.js
@@ -40,12 +40,21 @@ var FormFor = React.createClass({
     if(submitHandler) {
       var data = {};
       Object.keys(this.refs).forEach(function(ref) {
-        var value = this.refs[ref].refs.input.getDOMNode().value;
+        var value = this.getInputValue(ref);
         data[ref] = value;
       }.bind(this))
       console.log(data)
       submitHandler(data);
     }
+  },
+  getInputValue: function(ref) {
+    var input = undefined;
+    // find the ref component
+    // find the refs of the found component
+    // find the input ref from the found component
+    // get the getDOMNode and the value
+    if(this.refs[ref] && this.refs[ref].refs && this.refs[ref].refs.input && this.refs[ref].refs.input.getDOMNode) input = this.refs[ref].refs.input.getDOMNode().value;
+    return input;
   }
 
 });
