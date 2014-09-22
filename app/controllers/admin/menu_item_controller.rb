5c04ec1a93017d524c1a85ba661b5a57fe2b450b
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-22 16:09:37 -0700

    Deleted Spec file for menu item.

--- app/controllers/admin/menu_item_controller.rb
+++ app/controllers/admin/menu_item_controller.rb
@@ -1,6 +1,6 @@
 class Admin::MenuItemController < AdminController
   def index
-    render json: Menu_Item.all
+    # render json: Menu_Item.all
   end
 
   def create
--- spec/controllers/menu_item_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MenuItemController, :type => :controller do
-
-end
