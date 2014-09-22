ec62f72840b36cb59dd6dc27d4668a6606b1e99c
Gunnari Auvinen & Steven Leu <pair+cheerazar.eksentrysyti@devbootcamp.com>
2014-09-22 16:23:48 -0700

    Add user-address-form component.

---
+++ app/assets/javascripts/react/user-address-form.react.js
@@ -0,0 +1,46 @@
+/**
+ * @jsx React.DOM
+ */
+//= require react
+//= require react/form-builder/form-for.react
+//= require stores/address-store
+//= require actions/address-actions
+
+var UserAddressForm = React.createClass({displayName: 'UserAddressForm',
+  getInitialState: function() {
+    return {
+      errors: []
+    };
+  },
+
+  componentDidMount: function() {
+    AddressStore.addFailToTakeAction(function (e, data) {
+      if (this.isMounted()) this.setState({ errors: data });
+    }.bind(this));
+    AddressStore.addChangeEvent(function () {
+      if (this.isMounted()) this.setState({ errors: [] });
+    }.bind(this));
+  },
+
+  render: function () {
+    var address = this.props.address || AddressStore.new();
+    var formOptions = {
+      name: "Address",
+      onSubmit: this.handleSubmit
+    };
+
+    return (
+      React.DOM.div(null, 
+        FormFor({object: address, options: formOptions, errors: this.state.errors})
+      )
+    );
+  },
+
+  handleSubmit: function (data) {
+    if (this.props.editing) {
+      AddressActions.updateAddress(data)
+    } else {
+      AddressActions.createAddress(data)
+    }
+  }
+})
