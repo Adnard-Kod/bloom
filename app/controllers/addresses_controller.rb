c9adb1a47c271f9fc101d714b00493bde08f1a8a
Gunnari Auvinen & Steven Leu <pair+cheerazar.eksentrysyti@devbootcamp.com>
2014-09-23 10:39:56 -0700

    Add update and show methods to address controller.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,12 +2,32 @@ class AddressesController < UserController
   def create
     address = Address.new(address_params.merge(user_id: current_user.id))
     if address.save
-      render json: address
+      render json: { address: address }
     else
       render json: {errors: address.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
+  def update
+    address = Address.find(params[:address][:id])
+    address.update(address_params)
+    if address.save
+      render json: { address: address }
+    else
+      render json: { errors: address.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    address = Address.find_by_user_id(current_user.id)
+    if address.nil?
+      render json: { errors: ["An address doesn't exist for this user"]}, status: :unprocessable_entity
+    else
+      render json: { address: address}
+    end
+  end
+
+  private
   def address_params
     params.require(:address).permit(:street_address, :apartment_number, :city, :state, :zipcode, :delivery_instructions)
   end
