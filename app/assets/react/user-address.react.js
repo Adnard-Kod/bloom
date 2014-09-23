6cef32928e341709bbfd4f275d7e7185db032163
Gunnari Auvinen & Steven Leu <pair+cheerazar.eksentrysyti@devbootcamp.com>
2014-09-23 10:38:49 -0700

    Create user-address component.

---
+++ app/assets/react/user-address.react.js
@@ -0,0 +1,42 @@
+/** @jsx React.DOM */
+//= require react
+//= require stores/address-store
+//= require react/user-address-form.react
+//= require actions/address-actions
+
+var UserAddress = React.createClass({
+  getInitialState: function() {
+    return {
+      editing: false,
+    };
+  },
+
+  componentDidMount: function() {
+    AddressStore.addChangeEvent(function () {
+      if (this.isMounted()) this.setState({ editing: false });
+    }.bind(this));
+  },
+
+  render: function () {
+    var addr = this.props.addr;
+    var editForm = this.state.editing ? <UserAddressForm address={addr} editing="true" /> : undefined;
+    return (
+      <div className="col-lg-4">
+        <h3>Current Address</h3>
+        <ul className="list-group">
+          <li className="list-group-item">Name</li>
+          <li className="list-group-item">{addr.street_address}</li>
+          <li className="list-group-item">{addr.apartment_number}</li>
+          <li className="list-group-item">{addr.city}, {addr.state}, {addr.zipcode}</li>
+          <span><a href="#" onClick={this.edit}>Edit</a></span>
+          {editForm}
+        </ul>
+      </div>
+    )
+  },
+
+  edit: function (e) {
+    e.preventDefault();
+    this.setState({ editing: !this.state.editing });
+  }
+})
