6127b6636a1a651bf74c82ffc0a916bbec3f13e9
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-22 19:49:40 -0700

    Update the menu item migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20140922223937) do
   create_table "menu_items", force: true do |t|
     t.string   "name"
     t.string   "description"
-    t.string   "type"
+    t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
