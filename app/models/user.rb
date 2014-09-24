9e2c4ad86f641e16b6911f8c69cc79429077a3a3
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-24 10:11:47 -0700

    Add before_destroy method to user model to unsubscribe/delete user from the database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :memberships
   has_many :subscriptions, :through => :memberships
   after_create :subscribe_to_mailchimp
+  before_destroy :unsubscribe_to_mailchimp
 
   def subscribe_to_mailchimp testing=false
     return true if (Rails.env.test? && !testing)
@@ -15,6 +16,17 @@ class User < ActiveRecord::Base
       id: list_id,
       email: { email: email },
       double_optin: false
+      })
+    response
+  end
+
+  def unsubscribe_to_mailchimp testing=false
+    return true if (Rails.env.test? && !testing)
+    list_id = ENV['MAILCHIMP_LIST_ID']
+    response = Rails.configuration.mailchimp.lists.unsubscribe({
+      id: list_id,
+      email: { email: email },
+      delete_member: true
       })
     response
   end
