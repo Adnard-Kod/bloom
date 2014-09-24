b30f83ec45fa19d61fe5d3a7f2f39fedc6d0dc68
Gunnari Auvinen & Steven Leu <pair+cheerazar.eksentrysyti@devbootcamp.com>
2014-09-23 19:57:11 -0700

    Add helper_method to addresses_controller. Add SessionStore.currentUser to user dashboard index view.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,5 @@ class UserController < ActionController::Base
   include AuthenticationConcern
   before_filter :authenticate_user!
   protect_from_forgery with: :exception
+  helper_method :current_user
 end
