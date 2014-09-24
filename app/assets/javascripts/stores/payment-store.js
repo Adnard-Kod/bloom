a407919ffd3b8106dc519b2feb06fd906f11f8b5
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-24 12:40:10 -0700

    Update payment-store to pass payment-details to backend.

--- app/assets/javascripts/stores/payment-store.js
+++ app/assets/javascripts/stores/payment-store.js
@@ -1,21 +1,27 @@
 //= require stores/session-store
 
 var PaymentStore = (function () {
+  var paymentInfo = {
+    name: 'Blooming Spoon',
+    description: '2 widgets ($20.00)',
+    amount: 2000
+  };
+
   var handler = StripeCheckout.configure({
     key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
     image: '/assets/culinary/01_Blooming_Spoon_Logo_Final.png',
     token: function(token) {
-      PaymentStore.authorizePayment(token);
+      PaymentStore.authorizePayment(token, paymentInfo);
     }
   });
 
   return {
-    authorizePayment: function (token) {
+    authorizePayment: function (token, paymentInfo) {
       var authenticityToken = SessionStore.getAuthenticityToken();
         $.ajax({
           type: 'POST',
           url: '/user/dashboard/charge',
-          data: {token: token, authenticity_token: authenticityToken}
+          data: { token: token, authenticity_token: authenticityToken, payment_info: paymentInfo }
         })
         .done(function (data) {
         })
@@ -26,11 +32,7 @@ var PaymentStore = (function () {
     paymentListener: function () {
       $('#customButton').on('click', function(e) {
         // Open Checkout with further options
-        handler.open({
-          name: 'Blooming Spoon',
-          description: '2 widgets ($20.00)',
-          amount: 2000
-        });
+        handler.open(paymentInfo);
         e.preventDefault();
       });
     }
