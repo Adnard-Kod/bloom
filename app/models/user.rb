104a03ad4c71199e12f1395f8e0d18f6cb119674
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-23 17:58:41 -0700

    Update user model to include mailchimp functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ActiveRecord::Base
   has_many :addresses
   has_many :memberships
   has_many :subscriptions, :through => :memberships
+
+  def subscribe_to_mailchimp testing=false
+    return true if (Rails.env.test? && !testing)
+    list_id = '37bebf807f'
+    response = Rails.configuration.mailchimp.lists.subscribe({
+      id: list_id,
+      email: { email: email },
+      double_optin: false,
+      })
+    response
+  end
+
+  after_create do
+    subscribe_to_mailchimp
+  end
+
 end
