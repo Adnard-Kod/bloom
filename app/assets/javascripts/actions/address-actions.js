ca882b22b70cf80e10fa7ef6cf14a53256c07875
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-23 21:06:42 -0700

    Update address-actions to require sessions-store. Add _addUserId function to address-actions.

--- app/assets/javascripts/actions/address-actions.js
+++ app/assets/javascripts/actions/address-actions.js
@@ -1,25 +1,32 @@
 //= require dispatchers/blooming-dispatcher
 //= require constants/blooming-constants
+//= require stores/session-store
 
 var AddressActions = {
+  _addUserId: function (data) {
+    data.userId = SessionStore.currentUser;
+    return data;
+  },
+
   createAddress: function (data) {
     BloomingDispatcher.handleViewAction({
       type: BloomingConstants.ActionTypes.CREATE_ADDRESS,
-      data: data
+      data: this._addUserId(data)
     });
   },
 
   updateAddress: function (data) {
     BloomingDispatcher.handleViewAction({
       type: BloomingConstants.ActionTypes.UPDATE_ADDRESS,
-      data: data
+      data: this._addUserId(data)
     });
   },
 
   destroyAddress: function (id) {
+    var data = {id: id};
     BloomingDispatcher.handleViewAction({
       type: BloomingConstants.ActionTypes.DESTROY_ADDRESS,
-      id: id
+      data: this._addUserId(data)
     });
   }
 }
