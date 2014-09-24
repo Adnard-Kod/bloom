13192e1e66e3162ae9bb7f6fcd10d3a09e247005
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-24 16:27:14 -0700

    Update mailchimp test to match new first and last name expectations as well as 'update_existing' to be true

--- spec/mailchimp/mailchimp_spec.rb
+++ spec/mailchimp/mailchimp_spec.rb
@@ -5,8 +5,10 @@ describe "#subscribe_to_mailchimp" do
    it "calls mailchimp correctly" do
      opts = {
        email: {email: user.email},
+       merge_vars: {"FNAME" => user.first_name, "LNAME" => user.last_name},
        id: ENV['MAILCHIMP_LIST_ID'],
        double_optin: false,
+       update_existing: true
      }
      clazz = Rails.configuration.mailchimp.lists.class
      clazz.any_instance.should_receive(:subscribe).with(opts).once
