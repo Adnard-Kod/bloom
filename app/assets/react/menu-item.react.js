ec912de373d08ec9ea3da8ca7b88399829827850
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-25 15:22:11 -0700

    Change color of menu item panel and built in handling of link components

--- app/assets/react/menu-item.react.js
+++ app/assets/react/menu-item.react.js
@@ -25,14 +25,18 @@ var MenuItem = React.createClass({
   render: function() {
     var menuItem = this.props.menuItem;
     var editForm = this.state.editing ? <MenuItemForm menuItem={menuItem} editing="true"/> :undefined;
-    var panelClass = "panel panel-primary";
+    var panelClass = "panel panel-info";
+    var editLinks = [
+      {handler: this.edit, name: 'edit', className: 'text-warning'},
+      {handler: this.delete, name: 'delete', className: 'text-danger'}
+    ];
     if(this.props.active) panelClass = "panel panel-success"
     return (
       <div className={panelClass}>
         <div className="panel-heading">
           <h3 className="panel-title">
             {menuItem.name} ({menuItem.category})
-            <EditLinks edit={this.edit} delete={this.delete} />
+            <EditLinks links={editLinks} />
           </h3>
         </div>
         <div className="panel-body">
