2e935dcda0e83b47bbce7e510377fdaa9929e35d
Steven Leu & Gunnari Auvinen <pair+Eksentrysyti.cheerazar@devbootcamp.com>
2014-09-25 11:25:29 -0700

    Create payments controller and create action. Remove charge action from dashboard_controller.

---
+++ app/controllers/user/payments_controller.rb
@@ -0,0 +1,23 @@
+class User::PaymentsController < UserController
+  def create
+    # Set your secret key: remember to change this to your live secret key in production
+    # See your keys here https://dashboard.stripe.com/account
+    Stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+    # Get the stripe token generated from the submitted form
+    token = params[:token][:id]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => params[:payment_info][:amount].to_i, # amount in cents, again
+        :currency => "usd",
+        :card => token,
+        :description => "payinguser@example.com"
+      )
+    rescue Stripe::CardError => e
+      p "STRIPE ERROR #{e}"
+    end
+
+    render json: charge
+  end
+end
