bf42998f89c5b76e678a539411d8f03e55745897
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-25 11:35:38 -0700

    Add tests for current status menu.

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,11 +1,36 @@
 require 'rails_helper'
 describe Menu do
-  let(:menu) { FactoryGirl.build :menu }
   context "validations" do
+    let(:menu) { FactoryGirl.build :menu }
     it { should validate_presence_of :title }
     it { should have_many :selected_items }
     it { should have_many :items }
     it { should have_many :default_selected_items }
     it { should have_many(:default_items).through(:default_selected_items) }
+  end
+
+  context "Current Menu" do
+    let!(:menu) { FactoryGirl.create :menu, :current }
+    it "Should return one current menu." do
+      current_menu = Menu.current
+      expect(current_menu).to eq(menu)
+    end
+
+    it "Should reassign the current menu." do
+      menu = Menu.current
+      current_menu = FactoryGirl.create :menu, :current
+      expect( current_menu.current ).to eq(true)
+    end
+
+    it "Should change current attribute to true." do
+      new_menu = FactoryGirl.create :menu
+      new_menu.current!
+      expect(new_menu.current).to eq(true)
+    end
+
+    it "Should change current attribute to false." do
+      menu.decurrent!
+      expect(menu.current).to eq(false)
+    end
   end
 end
