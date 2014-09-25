c05554158b74c63480a3a0e8fe0ae1c0088f2b12
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-25 15:18:55 -0700

    Changed edit link component to be dynamic.

--- app/assets/javascripts/react/edit-links.react.js
+++ app/assets/javascripts/react/edit-links.react.js
@@ -5,13 +5,16 @@
 
 var EditLinks = React.createClass({displayName: 'EditLinks',
   render: function() {
+    var links = [];
+    this.props.links.forEach(function(link, panel, i) {
+      var className = link.className || 'text-default';
+      links.push(React.DOM.strong(null, React.DOM.a({href: "#", className: className, onClick: link.handler}, " ", link.name, " ")));
+    })
     return (
       React.DOM.span(null,
-        React.DOM.strong(null, React.DOM.a({href: "#", className: "text-warning", onClick: this.props.edit}, " edit |")),
-        React.DOM.strong(null, React.DOM.a({href: "#", className: "text-danger", onClick: this.props.delete}, " delete"))
+        links
       )
     );
   }
-
 });
 
