6845cb90d55309b075b23253579945474023751a
Ernie Salazar & Karma Hater! <ehsalazar75@gmail.com>
2014-09-24 18:29:58 -0700

    Add methods to select a particular menu as current

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,4 +4,23 @@ class Menu < ActiveRecord::Base
   has_many :items, :through => :selected_items, :source => :menu_item
   has_many :default_selected_items, ->{where(:default => true)}, :class_name => 'SelectedItem'
   has_many :default_items, :through => :default_selected_items, :source => :menu_item
+  has_many :menu_items
+  before_update :deselect_default
+
+  def self.current
+    self.find_by_current(true)
+  end
+
+  def deselect_default
+    current_menu = Menu.current
+    current_menu.decurrent! unless current_menu.blank? || current_menu == self
+  end
+
+  def decurrent!
+    self.update_attribute(:current, false)
+  end
+
 end
+
+
+
