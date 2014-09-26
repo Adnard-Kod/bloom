d042d10da0dca35eb4d7ba82523ce0e1ae48dd32
Steven Leu & Gunnari Auvinen <pair+Eksentrysyti.cheerazar@devbootcamp.com>
2014-09-25 17:02:11 -0700

    Add associations to membership model. Create methods to generate membership parameters during before_save callbacks.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,2 +1,22 @@
 class Membership < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :subscription
+  before_create :set_meals_and_weeks, :set_start_date
+
+  private
+  def set_meals_and_weeks
+    self.meals_remaining = self.subscription.meals
+    self.weeks_remaining = self.subscription.weeks
+    self.meals_per_week =  self.meals_remaining / self.weeks_remaining
+  end
+
+  def set_start_date
+    today = DateTime.now.to_date
+    self.start_date = if today.cwday == 7
+                        today + 7
+                      else
+                        days_from_sunday = 7 - today.cwday
+                        today + days_from_sunday
+                      end
+  end
 end
