ae7f1ef48ce534114f8693085c35c25452b048c4
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-29 16:39:36 -0700

    Update membership serializer to include subscription relationship.

--- app/serializers/membership_serializer.rb
+++ app/serializers/membership_serializer.rb
@@ -1,3 +1,4 @@
 class MembershipSerializer < ActiveModel::Serializer
   attributes :id, :weeks_remaining, :meals_remaining, :meals_per_week, :start_date, :end_date, :status
+  has_one :subscription
 end
