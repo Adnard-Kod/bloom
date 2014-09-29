db47c7a27138374c8319cfe0803b7e8bc67d1649
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-29 16:52:43 -0700

    Add show action to UsersController. Add load_and_authorize_user method to users controller and run on a before_filter for show method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :load_and_authorize_user, only: [:show]
+
   def create
     user = User.new(user_params)
     if user.save
@@ -9,8 +11,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    render json: @user
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :phone_number, :password, :password_confirmation)
+  end
+
+  def load_and_authorize_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless @user == current_user || current_user.admin?
   end
 end
