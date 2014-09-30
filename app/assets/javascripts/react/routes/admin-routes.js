f7185b47868dd93387e3ec89ca6399d95a28f207
Steven Leu <steven.c.leu@gmail.com>
2014-09-30 12:34:19 -0700

    Add routing for Promotions

--- app/assets/javascripts/react/routes/admin-routes.js
+++ app/assets/javascripts/react/routes/admin-routes.js
@@ -6,6 +6,7 @@
 //= require react/menu-item-box.react
 //= require react/subscriptions.react
 //= require react/add-on-box.react
+//= require react/promotions.react
 Router.routes = (function() {
   var _adminDashBoard = function() {
     React.renderComponent(AdminDashboard(null), $('#admin-page')[0])
@@ -19,11 +20,15 @@ Router.routes = (function() {
   var _addOnBox = function() {
     React.renderComponent(AddOnBox(null), $('#admin-page')[0])
   }
+  var _promotions = function() {
+    React.renderComponent(Promotions(null), $('#admin-page')[0])
+  }
   return {
     "": _adminDashBoard,
     "#menu-items": _menuItemsBox,
     "#subscriptions": _subscriptions,
-    "#add-ons": _addOnBox
+    "#add-ons": _addOnBox, 
+    "#promotions": _promotions
   }
 }());
 
