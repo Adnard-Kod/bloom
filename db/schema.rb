1afc852ba259f6a844d0347a5c167321ce5ff8e1
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-09-29 17:45:44 -0700

    Add active field to items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929184726) do
+ActiveRecord::Schema.define(version: 20140930004323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140929184726) do
     t.datetime "updated_at"
     t.integer  "price"
     t.string   "type"
+    t.boolean  "active",      default: false
   end
 
   create_table "memberships", force: true do |t|
