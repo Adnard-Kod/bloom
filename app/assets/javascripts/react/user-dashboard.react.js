2351d3770c3f4c0b58e1a7cb700cb5bf7c28e052
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-09-29 19:40:00 -0700

    add activeAddOn component

--- app/assets/javascripts/react/user-dashboard.react.js
+++ app/assets/javascripts/react/user-dashboard.react.js
@@ -1,15 +1,23 @@
 /** @jsx React.DOM */
 //= require react
 //= require react/current-menu.react
+//= require react/active-add-ons.react
+
 var UserDashboard = React.createClass({displayName: 'UserDashboard',
   render: function () {
     return (
+<<<<<<< HEAD
       React.DOM.div({className: "container-fluid"},
         React.DOM.div({className: "row"},
           React.DOM.div({className: "col-lg-12"},
             CurrentMenu(null)
           )
         )
+=======
+      React.DOM.div(null,
+        CurrentMenu(null),
+        ActiveAddOns(null)
+>>>>>>> add activeAddOn component
       )
     )
   }
