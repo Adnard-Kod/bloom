f48c60bf413f81ade7eb6de44ccff1aeea17d4b8
Steven Leu <steven.c.leu@gmail.com>
2014-09-30 12:46:51 -0700

    Add components for PromotionForm, Promotion, Promotions (not fully completed yet)

---
+++ app/assets/react/promotion.react.js
@@ -0,0 +1,56 @@
+/**
+ * @jsx React.DOM
+ */
+ //= require stores/promotion-store
+//= require react/promotion-form.react
+//= require actions/promotion-actions
+//= require react/edit-links.react
+
+ var Promotion = React.createClass({
+  getInitialState: function() {
+    return {
+      editing: false
+    };
+  },
+  getDefaultProps: function() {
+    return {
+      admin: false
+    };
+  },
+  componentDidMount: function() {
+    PromotionStore.addChangeEvent(function() {
+      if(this.isMounted()) this.setState({editing: false});
+    }.bind(this))
+  },
+  render: function() {
+    var promo = this.props.promo;
+    return (
+      <li className="list-group-item">
+        <p>{promo.name}: {promo.description} for ${promo.price}</p>
+        {this.renderAdminButtons()}
+        {this.renderEditForm()}
+      </li>
+    );
+  },
+  renderAdminButtons: function() {
+    if(this.props.admin) {
+      var editLinks = [
+        {handler: this.edit, name: 'edit', className: 'text-warning'},
+        {handler: this.delete, name: 'delete', className: 'text-danger'}
+      ];
+      return(<EditLinks links={editLinks} />);
+    }
+  },
+  renderEditForm: function() {
+    if (this.state.editing) return(<PromotionForm promotion={this.props.promo} editing="true"/>);
+  },
+  edit: function(e) {
+    e.preventDefault();
+    this.setState({editing: !this.state.editing})
+  },
+  delete: function(e) {
+    e.preventDefault();
+    PromotionActions.destroyPromotion(this.props.promo.id);
+  }
+ })
+
