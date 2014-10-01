7216ccaa4d03e786b9e380ca80cdc55da576516e
Steven Leu <steven.c.leu@gmail.com>
2014-09-30 18:14:10 -0700

    Add compiled javascript/react files for promotion components

---
+++ app/assets/javascripts/react/promotion-form.react.js
@@ -0,0 +1,45 @@
+/**
+ * @jsx React.DOM
+ */
+//= require react
+//= require stores/promotion-store
+//= require react/form-builder/form-for.react
+//= require stores/promotion-store
+//= require actions/promotion-actions
+var PromotionForm = React.createClass({displayName: 'PromotionForm',
+  getInitialState: function() {
+    return {
+      errors: []
+    };
+  },
+  componentDidMount: function() {
+    PromotionStore.addFailToTakeAction(function(e, data) {
+      if(this.isMounted()) this.setState({errors: data});
+    }.bind(this))
+    PromotionStore.addChangeEvent(function() {
+      if(this.isMounted()) this.setState({errors: []});
+    }.bind(this))
+  },
+  render: function() {
+    var promotion = this.props.promotion || PromotionStore.new();
+    var formOptions = {
+      name: "Promotion",
+      onSubmit: this.handleSubmit,
+      description: {
+        type: 'textarea'
+      }
+    }
+    return (
+      React.DOM.div(null,
+        FormFor({object: promotion, options: formOptions, errors: this.state.errors})
+      )
+    );
+  },
+  handleSubmit: function(data) {
+    if(this.props.editing) {
+      PromotionActions.updatePromotion(data)
+    } else {
+      PromotionActions.createPromotion(data)
+    }
+  }
+})
