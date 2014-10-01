04a32088578632b74f3b8003c961806bf7885644
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-09-30 19:55:32 -0700

    wrote active method to return the active add on

--- app/models/add_on.rb
+++ app/models/add_on.rb
@@ -2,4 +2,8 @@ class AddOn < Item
   validates :name, :description, :price, presence: true
   has_many :add_on_users
   has_many :users, through: :add_on_users
+
+  def self.active_add_on
+    return AddOn.where(active: true)
+  end
 end
