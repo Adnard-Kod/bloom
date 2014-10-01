d99c0f86ff265cdd47ed897be02f0c2fc1149bb3
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-30 17:39:30 -0700

    Add memberships component.

---
+++ app/assets/react/memberships.react.js
@@ -0,0 +1,38 @@
+/**
+ * @jsx React.DOM
+ */
+//= require react
+//= require stores/membership-store
+//= require react/membership.react
+
+var Memberships = React.createClass({
+  getInitialState: function() {
+    return {
+      memberships: MembershipStore.memberships()
+    };
+  },
+
+  componentDidMount: function() {
+    MembershipStore.addChangeEvent(function() {
+      this.setState({
+        memberships: MembershipStore.memberships()
+      });
+    }.bind(this));
+    MembershipStore.getUserMemberships();
+  },
+
+  render: function() {
+    var memberships = [];
+    this.state.memberships.forEach(function(mem) {
+      memberships.push(<Membership membership={mem} key={mem.id} />);
+    });
+    return (
+      <div>
+        <h4>Order History</h4>
+        <ul className="list-group">
+          {memberships}
+        </ul>
+      </div>
+    );
+  }
+})
