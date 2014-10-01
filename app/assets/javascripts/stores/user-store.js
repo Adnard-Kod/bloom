14521a4b37b8517f88f4f34cf91fdb5eb11435e4
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-01 15:56:04 -0700

    add update, all and destory ajax methods.

--- app/assets/javascripts/stores/user-store.js
+++ app/assets/javascripts/stores/user-store.js
@@ -3,12 +3,16 @@
 
 var UserStore = (function () {
   var _user = {};
+  var _users = [];
   var CHANGE_EVENT = 'change';
   var ActionTypes = BloomingConstants.ActionTypes;
 
   return {
     currentUser: function() {
       return _user;
+    },
+    users: function() {
+      return _users;
     },
     new: function() {
       return {
@@ -20,14 +24,12 @@ var UserStore = (function () {
         password_confirmation: null
       }
     },
-
     addPropertyToUser: function(key, properties) {
       if(key in _user) {
         _user[key] = [properties];
         this.triggerChange();
       }
     },
-
     removePropertyFromUser: function(key, properties) {
       if (key in _user) {
         _user[key].forEach(function(addr, index) {
@@ -38,9 +40,7 @@ var UserStore = (function () {
         }.bind(this))
       }
     },
-
     create: function (userData) {
-
       $.ajax({
         type: 'POST',
         url: '/users',
@@ -53,7 +53,52 @@ var UserStore = (function () {
         $(this).trigger('creation-error', JSON.parse(xhr.responseText));
       }.bind(this));
     },
-
+    all: function(add_on_id) {
+      $.ajax({
+        url: '/admin/users',
+        type: 'GET',
+      })
+      .done(function(data) {
+        _users = data.users;
+        this.triggerChange();
+      }.bind(this))
+    },
+    destroy: function(id) {
+      $.ajax({
+        url: '/admin/users/'+id,
+        type: 'DELETE',
+        data: {id: id}
+      })
+      .done(function(data) {
+        _users.forEach(function(user, i) {
+          if (user.id === data.id) {
+            _users.splice(i, 1);
+            return this.triggerChange();
+          }
+        }.bind(this))
+      }.bind(this))
+      .fail(function(xhr) {
+        this.triggerFailToTakeAction([xhr.responseJSON.errors]);
+      }.bind(this))
+    },
+    update: function(user) {
+      $.ajax({
+        url: '/admin/users/'+ user.id,
+        type: 'PUT',
+        data: {user: user}
+      })
+     .done(function(data) {
+       _users.forEach(function(user, i) {
+         if(user.id === data.user.id) {
+           _users[i]['admin'] = data.user.admin;
+           return this.triggerChange();
+         }
+       }.bind(this))
+     }.bind(this))
+     .fail(function(xhr) {
+       this.triggerFailToTakeAction([xhr.responseJSON.errors]);
+     }.bind(this))
+     },
     getCurrentUserInfo: function(userId) {
       $.ajax({
         type: 'GET',
@@ -64,19 +109,23 @@ var UserStore = (function () {
         this.triggerChange();
       }.bind(this))
     },
-
     addChangeEvent: function(callback) {
       $(this).on(CHANGE_EVENT, callback);
     },
     triggerChange: function(data) {
       $(this).trigger(CHANGE_EVENT, data);
     },
-
     payload: function(payload) {
       var action = payload.action;
       switch(action.type) {
         case ActionTypes.CREATE_USER:
           this.create(action.data);
+          break;
+        case ActionTypes.DESTROY_USER:
+          this.destroy(action.id)
+          break;
+        case ActionTypes.UPDATE_USER:
+          this.update(action.data)
           break;
         default:
           // do nothing
