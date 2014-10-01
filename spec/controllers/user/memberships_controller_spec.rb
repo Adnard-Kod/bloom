19647f4104d6b72e5d738569f5402a8440cf610a
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-01 11:29:43 -0700

    Create context for expired subscriptions in user memberships controller. Create an expired membership and force the load of it with the bang.

--- spec/controllers/user/memberships_controller_spec.rb
+++ spec/controllers/user/memberships_controller_spec.rb
@@ -2,25 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User::MembershipsController, :type => :controller do
   let(:user) { FactoryGirl.create :user }
-  let(:subscription) { FactoryGirl.create :subscription}
+
   before(:each) do
     stub_current_user controller, user
   end
 
   context "#index" do
-    before(:each) do
-      membership_expired = user.memberships.new :subscription => subscription
-      membership_expired.expired!
-      membership_expired.save
+    context "expired memberships" do
+      let!(:expired_membership) { FactoryGirl.create :membership, :expired, user: user }
 
-      membership = user.memberships.new :subscription => subscription
-      membership.expired!
-      membership.save
-    end
-
-    it "returns all expired memberships for current user" do
-      get :index, user_id: user
-      expect(JSON.parse(response.body).count).to eq(1)
+      it "returns all expired memberships for current user" do
+        get :index, user_id: user
+        expect(JSON.parse(response.body)['memberships'].length).to eq(1)
+      end
     end
   end
 end
