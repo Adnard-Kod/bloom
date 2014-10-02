80f6eb1fd213c7487e1fe8c42e403959586a3fd3
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-01 17:29:05 -0700

    Update admin memberships controller to return all memberships grouped by status.

--- app/controllers/admin/memberships_controller.rb
+++ app/controllers/admin/memberships_controller.rb
@@ -1,5 +1,7 @@
 class Admin::MembershipsController < AdminController
   def index
-    render json: Membership.active
+    memberships = ActiveModel::ArraySerializer.new(Membership.all).as_json
+    grouped_memberships = memberships.group_by {|mem| mem[:status]}
+    render json: grouped_memberships
   end
 end
