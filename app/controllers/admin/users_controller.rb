70acde4cbf96a22bc6d97f9e0c6ba195f0d51573
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-01 17:23:23 -0700

    fix the broken controller code in update and destroy

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,17 @@
 class Admin::UsersController < AdminController
-
   def index
     render json: User.order('admin desc')
   end
 
   def update
-    render json: User.find(params[:id]).update_attributes(user_params)
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    render json: user
   end
 
   def destroy
-    render json: User.find(params[:id]).destroy
+    user = User.find(params[:id]).destroy
+    render json: {id: user.id}
   end
 
 
