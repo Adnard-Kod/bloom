7ed63abde9a32f4ef4f702fc3793adefe6efcc83
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-01 19:04:38 -0700

    Require and render tab component in tablist.

--- app/assets/javascripts/react/tabs/tablist.react.js
+++ app/assets/javascripts/react/tabs/tablist.react.js
@@ -2,11 +2,20 @@
  * @jsx React.DOM
  */
 //= require react
+//= require react/tabs/tab.react
 
 var Tablist = React.createClass({displayName: 'Tablist',
   render: function() {
+    var tabs = [];
+    this.props.tabs.forEach(function(tab, index) {
+      if (index === 0) classActive = 'active';
+      tabs.push(Tab({active: classActive, tab: this.props.tabs[index]}));
+    }.bind(this));
+
     return (
-      React.DOM.div(null)
+      React.DOM.ul({className: "nav nav-tabs", role: "tablist"}, 
+        tabs
+      )
     );
   }
 })
--- app/assets/react/tabs/tablist.react.js
+++ app/assets/react/tabs/tablist.react.js
@@ -2,11 +2,20 @@
  * @jsx React.DOM
  */
 //= require react
+//= require react/tabs/tab.react
 
 var Tablist = React.createClass({
   render: function() {
+    var tabs = [];
+    this.props.tabs.forEach(function(tab, index) {
+      if (index === 0) classActive = 'active';
+      tabs.push(<Tab active={classActive} tab={this.props.tabs[index]} />);
+    }.bind(this));
+
     return (
-      <div />
+      <ul className="nav nav-tabs" role="tablist">
+        {tabs}
+      </ul>
     );
   }
 })
