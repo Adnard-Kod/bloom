fea5c06eae0aaaec155de2db91c30fe6b7c70659
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-01 17:25:13 -0700

    Refactor membership component for readability, by adding functions membershipActive, renderActiveMembershipHeader, and renderUserId.

--- app/assets/javascripts/react/membership.react.js
+++ app/assets/javascripts/react/membership.react.js
@@ -12,17 +12,33 @@ var Membership = React.createClass({displayName: 'Membership',
 
   render: function() {
     var mem = this.props.membership;
-    var activeMembershipDetails = mem.status === 'active' ? React.DOM.h3(null, "Active Membership Details") : undefined;
     return (
       React.DOM.div(null,
-        activeMembershipDetails,
+        this.renderActiveMembershipHeader(),
+        React.DOM.ul(null,
+          this.renderUserId(),
           React.DOM.li({className: "list-group-item"}, "Weeks Remaining: ", mem.weeks_remaining),
           React.DOM.li({className: "list-group-item"}, "Meals Remaining: ", mem.meals_remaining),
           React.DOM.li({className: "list-group-item"}, "Meals per Week: ", mem.meals_per_week),
           React.DOM.li({className: "list-group-item"}, "Start Date: ", mem.start_date),
           React.DOM.li({className: "list-group-item"}, "End Date: ", mem.end_date),
           React.DOM.li({className: "list-group-item"}, "Status: ", mem.status)
+        )
       )
     );
+  },
+
+  membershipActive: function() {
+    return this.props.membership.status && this.props.membership.status === 'active';
+  },
+
+  renderActiveMembershipHeader: function() {
+    if(this.membershipActive() && !this.props.admin) {
+      return (React.DOM.h4(null, "Active Membership Details"));
+    }
+  },
+
+  renderUserId: function() {
+    if(this.props.admin) return (React.DOM.li({className: "list-group-item"}, "User Id: ", this.props.membership.id));
   }
 });
--- app/assets/react/membership.react.js
+++ app/assets/react/membership.react.js
@@ -12,17 +12,33 @@ var Membership = React.createClass({
 
   render: function() {
     var mem = this.props.membership;
-    var activeMembershipDetails = mem.status === 'active' ? <h3>Active Membership Details</h3> : undefined;
     return (
       <div>
-        {activeMembershipDetails}
+        {this.renderActiveMembershipHeader()}
+        <ul>
+          {this.renderUserId()}
           <li className="list-group-item">Weeks Remaining: {mem.weeks_remaining}</li>
           <li className="list-group-item">Meals Remaining: {mem.meals_remaining}</li>
           <li className="list-group-item">Meals per Week: {mem.meals_per_week}</li>
           <li className="list-group-item">Start Date: {mem.start_date}</li>
           <li className="list-group-item">End Date: {mem.end_date}</li>
           <li className="list-group-item">Status: {mem.status}</li>
+        </ul>
       </div>
     );
+  },
+
+  membershipActive: function() {
+    return this.props.membership.status && this.props.membership.status === 'active';
+  },
+
+  renderActiveMembershipHeader: function() {
+    if(this.membershipActive() && !this.props.admin) {
+      return (<h4>Active Membership Details</h4>);
+    }
+  },
+
+  renderUserId: function() {
+    if(this.props.admin) return (<li className="list-group-item">User Id: {this.props.membership.id}</li>);
   }
 });
