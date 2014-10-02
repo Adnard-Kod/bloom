e87f6ecc1c51beaa0349cd7676d599edf5a95a74
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-01 19:34:40 -0700

    add method to return the number of each item for each week.

--- app/models/user_selected_item.rb
+++ app/models/user_selected_item.rb
@@ -2,4 +2,10 @@ class UserSelectedItem < SelectedItem
   attr_accessor :quantity
   scope :default, ->{where(:default => true)}
   belongs_to :user
+
+  def self.group_by_menu_item
+    sum = []
+    self.all.group_by(&:menu_item_id).each_value {|v| v.first.quantity = v.length; sum.push(v.first)}
+    sum.map {|selected| {:item => selected.menu_item.name, :quantity => selected.quantity}}
+  end
 end
