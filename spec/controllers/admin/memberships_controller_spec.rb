a794e8fef4a1cb001c568b8e98b782ff071d5886
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-02 10:39:37 -0700

    Update tests in admin controller to test for no memberships in the database, as well as active, expired, and on_hold memberships.

--- spec/controllers/admin/memberships_controller_spec.rb
+++ spec/controllers/admin/memberships_controller_spec.rb
@@ -8,13 +8,24 @@ RSpec.describe Admin::MembershipsController, :type => :controller do
   end
 
   context "#index" do
-    let!(:active_membership) { FactoryGirl.create :membership, :active }
-    let!(:on_hold_membership) { FactoryGirl.create :membership, :on_hold }
-    let!(:expired_membership) { FactoryGirl.create :membership, :expired }
+    context "active, on_hold, and expired memberships in the database" do
+      let!(:active_membership) { FactoryGirl.create :membership, :active }
+      let!(:on_hold_membership) { FactoryGirl.create :membership, :on_hold }
+      let!(:expired_membership) { FactoryGirl.create :membership, :expired }
 
-    xit "returns json with the membership grouped by statuses" do
-      get :index
-      expect(JSON.parse(response.body)).to eq({"active" => [active_membership], "on_hold" => [], "expired" => []})
+      it "returns json with the membership grouped by statuses" do
+        memberships = ActiveModel::ArraySerializer.new(Membership.all).as_json
+        grouped_memberships = memberships.group_by {|mem| mem[:status]}.as_json
+        get :index
+        expect(JSON.parse(response.body)).to eq(grouped_memberships)
+      end
+    end
+
+    context "no memberships in the database" do
+      it "returns an empty hash" do
+        get :index
+        expect(JSON.parse(response.body)).to eq({})
+      end
     end
   end
 end
