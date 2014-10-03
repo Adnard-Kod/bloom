8886b9339fb8e1650c8177c7d62018b6735e2433
Ed Shadi <edshadi@gmail.com>
2014-10-03 11:47:06 -0700

    test the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,13 @@ class User < ActiveRecord::Base
   Membership::STATUSES.keys.each do |status|
     has_many :"#{status}_memberships", -> { send status }, :class_name => 'Membership'
   end
-  after_create :subscribe_to_mailchimp
-  after_save :subscribe_to_mailchimp
+
+  after_save :subscribe_to_mailchimp, ->{self.email_changed?}
   before_destroy :unsubscribe_to_mailchimp
-  delegate :subscribe_to_mailchimp, :unsubscribe_to_mailchimp, :to => :mailchimp
   has_many :add_on_users
   has_many :add_ons, through: :add_on_users
+
+  delegate :subscribe_to_mailchimp, :unsubscribe_to_mailchimp, :to => :mailchimp
 
   def mailchimp
     @mailchimp ||= MailChimp.new self
@@ -32,6 +33,18 @@ class User < ActiveRecord::Base
     ActiveModel::ArraySerializer.new(self.current_orders).as_json
   end
 
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def current_address
+    self.addresses.first
+  end
+
+  def reduce_to_name_and_address
+    {:name => self.full_name, :address => self.current_address.try(:full)}
+  end
+
   def find_or_create_selected_items(default_selected_items)
     if self.selected_items.blank? && self.active_subscription.present?
       default_selected_items.each do |selected_item|
@@ -40,11 +53,13 @@ class User < ActiveRecord::Base
     end
     self.grouped_selected_items
   end
+
   def grouped_selected_items
     sum = []
     self.selected_items.group_by(&:menu_item_id).each_value {|v| v.first.quantity = v.length; sum.push(v.first)}
     sum
   end
+
   def replace_selected_items! items
     self.default_selected_items.destroy_all
     items.each do |item_id, quantity|
@@ -53,8 +68,5 @@ class User < ActiveRecord::Base
         self.selected_items.create :menu_item => item, :default => true
       }
     end
-  end
-  def full_name
-    "#{self.first_name} #{self.last_name}"
   end
 end
