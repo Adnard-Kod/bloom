71e93fe617a04d7eececeee83ac47e79ed6ed356
Ed Shadi <edshadi@gmail.com>
2014-10-03 12:23:32 -0700

    added reduce_to_user_and_menu_items in user selected item model and fully tested the model

--- app/models/user_selected_item.rb
+++ app/models/user_selected_item.rb
@@ -2,15 +2,27 @@ class UserSelectedItem < SelectedItem
   attr_accessor :quantity
   scope :default, ->{where(:default => true)}
   belongs_to :user
+  belongs_to :menu_item
 
   def self.group_by_menu_item
     sum = []
     self.all.group_by(&:menu_item_id).each_value {|v| v.first.quantity = v.length; sum.push(v.first)}
-    sum.map {|selected| {:item => selected.menu_item.name, :quantity => selected.quantity}}
+    sum
   end
 
-  def self.user_and_menu_items
+  def self.total_orders
+    self.group_by_menu_item.map &:reduce_to_item_and_quantity
+  end
 
+  def reduce_to_item_and_quantity
+    {:item => self.menu_item.name, :quantity => self.quantity}
+  end
+
+  def self.reduce_to_user_and_menu_items
+    self.includes(:menu_item, :user => [:addresses]).group_by_menu_item.group_by(&:user).reduce([]) do |sum, (user, items)|
+      reduced_user = user.reduce_to_name_and_address
+      sum.push({:name => reduced_user[:name], :address => reduced_user[:address], :menu_items => items.map(&:reduce_to_item_and_quantity)})
+    end
   end
 
 end
---
+++ spec/models/user_selected_item_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+describe UserSelectedItem do
+  let!(:user_selected_item) { FactoryGirl.create :user_selected_item }
+  context "associations" do
+    [:user, :menu_item].each do |assoc|
+      it { should belong_to assoc }
+    end
+  end
+  it ".default scope" do
+    default = FactoryGirl.create :user_selected_item, :default
+    expect(UserSelectedItem.default).to eq [default]
+  end
+  it ".group_by_menu_item" do
+    FactoryGirl.create :user_selected_item, :menu_item => user_selected_item.menu_item
+    expect(UserSelectedItem.group_by_menu_item).to eq [user_selected_item]
+    expect(UserSelectedItem.group_by_menu_item.first.quantity).to eq 2
+  end
+  it ".total_orders" do
+    expect_any_instance_of(UserSelectedItem).to receive(:reduce_to_item_and_quantity).once
+    UserSelectedItem.total_orders
+  end
+  it "#reduce_to_item_and_quantity" do
+    expect(user_selected_item.reduce_to_item_and_quantity).to eq({:item => user_selected_item.menu_item.name, :quantity => user_selected_item.quantity})
+  end
+  it ".reduce_to_user_and_menu_items" do
+    user = user_selected_item.user
+    grouped = UserSelectedItem.group_by_menu_item
+    reduced = {:name => user.full_name, :address => user.current_address.try(:full), :menu_items => grouped.map(&:reduce_to_item_and_quantity)}
+    expect(UserSelectedItem.reduce_to_user_and_menu_items).to eq([reduced])
+  end
+end
