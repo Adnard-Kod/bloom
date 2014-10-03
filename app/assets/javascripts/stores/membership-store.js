eef9940d8f81717acb18f3d032b0e908e33a9201
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-03 13:04:13 -0700

    Add additional functions to membership store to handle removing events and failures. Add update function and update payload to reflect new action.

--- app/assets/javascripts/stores/membership-store.js
+++ app/assets/javascripts/stores/membership-store.js
@@ -1,10 +1,13 @@
 //= require constants/blooming-constants
 //= require dispatchers/blooming-dispatcher
 //= require stores/session-store
+//= require stores/user-store
 
 var MembershipStore = (function() {
   var _memberships = [];
   var CHANGE_EVENT = 'change';
+  var FAIL_TO_CREATE_EVENT = 'creation-failed';
+  var ActionTypes = BloomingConstants.ActionTypes;
 
   return {
     memberships: function() {
@@ -37,15 +40,42 @@ var MembershipStore = (function() {
     addChangeEvent: function(callback) {
       $(this).on(CHANGE_EVENT, callback);
     },
-
+    removeChangeEvent: function(obj) {
+      $(this).off(CHANGE_EVENT, obj);
+    },
+    addFailToTakeAction: function(callback) {
+      $(this).on(FAIL_TO_CREATE_EVENT, callback);
+    },
+    removeFailToTakeAction: function(obj) {
+      $(this).off(FAIL_TO_CREATE_EVENT, obj);
+    },
+    triggerFailToTakeAction: function(data) {
+      $(this).trigger(FAIL_TO_CREATE_EVENT, data);
+    },
     triggerChange: function(data) {
       $(this).trigger(CHANGE_EVENT, data);
+    },
+
+    update: function(data) {
+      $.ajax({
+        type: 'PUT',
+        url: '/user/memberships/' + data.membershipId,
+        data: { user_id: data.userId, status: data.status }
+      })
+      .done(function(data) {
+        UserStore.removePropertyFromUser('active_memberships', data.membership);
+        UserStore.addPropertyToUser('on_hold_memberships', data.membership);
+      });
     },
 
     payload: function(payload) {
       var action = payload.action;
       switch(action.type) {
+        case ActionTypes.UPDATE_MEMBERSHIP:
+          this.update(action.data);
+          break;
         default:
+          // do nothing
       }
     }
   }
