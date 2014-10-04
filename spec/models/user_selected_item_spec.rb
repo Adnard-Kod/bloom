73f25f2251efe966cb723417953ca16f17cf26a0
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-03 17:22:20 -0700

    corrected tests to reflect the changes to user selected item model

--- spec/models/user_selected_item_spec.rb
+++ spec/models/user_selected_item_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 describe UserSelectedItem do
   let!(:user_selected_item) { FactoryGirl.create :user_selected_item, :default }
+  let(:user) { user_selected_item.user }
+  let(:menu_item) { user_selected_item.menu_item }
   context "associations" do
     [:user, :menu_item].each do |assoc|
       it { should belong_to assoc }
@@ -23,10 +25,30 @@ describe UserSelectedItem do
   it "#reduce_to_item_and_quantity" do
     expect(user_selected_item.reduce_to_item_and_quantity).to eq({:item => user_selected_item.menu_item.name, :quantity => user_selected_item.quantity})
   end
-  it ".reduce_to_user_and_menu_items" do
-    user = user_selected_item.user
-    grouped = UserSelectedItem.group_by_menu_item
-    reduced = {:name => user.full_name, :address => user.current_address.try(:full), :menu_items => grouped.map(&:reduce_to_item_and_quantity)}
-    expect(UserSelectedItem.reduce_to_user_and_menu_items).to eq([reduced])
+  it ".reduce_to_menu_item" do
+    FactoryGirl.create :user_selected_item, :default, :user => user, :menu_item => menu_item
+    FactoryGirl.create :user_selected_item, :default, :menu_item => menu_item
+
+    expect(UserSelectedItem.reduce_to_menu_item(user.default_selected_items)).to eq([user_selected_item])
+    expect(UserSelectedItem.reduce_to_menu_item(user.default_selected_items).first.quantity).to eq 2
+  end
+  context ".reduce_to_user_and_menu_items" do
+    before(:each) do
+      # create another selected item for the original user and origin item (user now selected quantity 2 of menu_item)
+      FactoryGirl.create :user_selected_item, :default, :user => user, :menu_item => menu_item
+      # create another selected item for the same user with different menu_item choice
+      FactoryGirl.create :user_selected_item, :default, :user => user
+    end
+    let!(:user_2_selected_item) { FactoryGirl.create :user_selected_item, :default, :menu_item => menu_item }
+    let(:user_2) { user_2_selected_item.user }
+    it ".reduce_to_user_and_menu_items" do
+      user_grouped = UserSelectedItem.reduce_to_menu_item user.default_selected_items
+      user_2_grouped = UserSelectedItem.reduce_to_menu_item user_2.default_selected_items
+      reduced = [
+        {:name => user.full_name, :address => user.current_address.try(:full), :menu_items => user_grouped.map(&:reduce_to_item_and_quantity)},
+        {:name => user_2.full_name, :address => user_2.current_address.try(:full), :menu_items => user_2_grouped.map(&:reduce_to_item_and_quantity)}
+      ]
+      expect(UserSelectedItem.reduce_to_user_and_menu_items).to eq(reduced)
+    end
   end
 end
