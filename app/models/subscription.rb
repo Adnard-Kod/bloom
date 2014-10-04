12369ebfb9bc8347af942cf8e5e47690cc37dce2
Ernie Salazar <ehsalazar@gmail.com>
2014-10-03 18:56:17 -0700

    Add validation for presence of name in the subscription model.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ActiveRecord::Base
-  validates :price, :meals, :weeks, :presence => true
+  validates :price, :meals, :weeks, :name, :presence => true
   validates :price, :meals, :weeks, :numericality => true
   has_many :memberships
   has_many :users, :through => :memberships
