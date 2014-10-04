b2ab030c85be3134352f70a70dc4dbacc8ada1f2
Ernie Salazar <ehsalazar@gmail.com>
2014-10-03 18:40:34 -0700

    Remove name method from subscription model. Client can enter name via subscription form.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,11 +1,6 @@
 class Subscription < ActiveRecord::Base
   validates :price, :meals, :weeks, :presence => true
   validates :price, :meals, :weeks, :numericality => true
-  before_validation :create_name
   has_many :memberships
   has_many :users, :through => :memberships
-  private
-  def create_name
-    self.name = "#{self.meals} meals for #{self.weeks} weeks"
-  end
 end
