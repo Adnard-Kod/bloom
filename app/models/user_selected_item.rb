f3e7bda081768e4fb83f3f899da1c4a8239da48b
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-03 17:20:44 -0700

    refactored methods to return data from the server.

--- app/models/user_selected_item.rb
+++ app/models/user_selected_item.rb
@@ -5,9 +5,7 @@ class UserSelectedItem < SelectedItem
   belongs_to :menu_item
 
   def self.group_by_menu_item
-    sum = []
-    self.default.group_by(&:menu_item_id).each_value {|v| v.first.quantity = v.length; sum.push(v.first)}
-    sum
+    self.reduce_to_menu_item self.default
   end
 
   def self.total_orders
@@ -18,10 +16,17 @@ class UserSelectedItem < SelectedItem
     {:item => self.menu_item.name, :quantity => self.quantity}
   end
 
+  def self.reduce_to_menu_item items
+    sum = []
+    items.group_by(&:menu_item_id).each_value {|v| v.first.quantity = v.length; sum.push(v.first)}
+    sum
+  end
+
   def self.reduce_to_user_and_menu_items
-    self.includes(:menu_item, :user => [:addresses]).group_by_menu_item.group_by(&:user).reduce([]) do |sum, (user, items)|
+    self.includes(:menu_item, :user => [:addresses]).group_by(&:user).reduce([]) do |sum, (user, items)|
       reduced_user = user.reduce_to_name_and_address
-      sum.push({:name => reduced_user[:name], :address => reduced_user[:address], :menu_items => items.map(&:reduce_to_item_and_quantity)})
+      grouped = self.reduce_to_menu_item items
+      sum.push({:name => reduced_user[:name], :address => reduced_user[:address], :menu_items => grouped.map(&:reduce_to_item_and_quantity)})
     end
   end
 
