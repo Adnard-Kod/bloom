52679c2a21bbd4983bccfa7936231b4aa29181f2
Steven Leu <steven.c.leu@gmail.com>
2014-10-03 18:59:08 -0700

    Add serializer function to convert price from pennies back to dollars when database information is queried for subscriptions and addons

--- app/serializers/add_on_serializer.rb
+++ app/serializers/add_on_serializer.rb
@@ -1,3 +1,9 @@
 class AddOnSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :price, :active, :type
+
+  def attributes
+    data = super
+    data[:price] = data[:price] / 100.00
+    data
+  end
 end
