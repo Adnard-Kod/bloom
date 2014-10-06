da3c11cc3bec3ebbcf320c8e2af6a7b0278b1fc9
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-06 15:51:51 -0700

    Add initial membership-hold-info component.

---
+++ app/assets/javascripts/react/membership-hold-info.react.js
@@ -0,0 +1,12 @@
+/**
+ * @jsx React.DOM
+ */
+//= require react
+
+var MembershipHoldInfo = React.createClass({displayName: 'MembershipHoldInfo',
+  render: function() {
+    return (
+      React.DOM.div(null)
+    );
+  }
+});
