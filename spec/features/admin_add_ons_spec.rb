49a5e053cc3f3ed063d8a3942a7d8f89b152b066
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-06 13:10:17 -0700

    fix active test

--- spec/features/admin_add_ons_spec.rb
+++ spec/features/admin_add_ons_spec.rb
@@ -13,37 +13,75 @@ describe "Admin Add Ons", :js => true do
     expect(page).to have_content(price_in_dollars)
   end
 
-  it "can create Add On" do
-    visit admin_dashboard_index_path
-    click_on "Add On Items"
-    fill_in "Name", with: "Add On Name"
-    fill_in "Description", with: "Add On Description"
-    fill_in "Price", with: "10"
-    click_on "Create Add On Item"
-    wait_for_ajax_to_finish
-    expect(page).to have_content("Add On Name")
-    expect(page).to have_content("Add On Description")
-    expect(page).to have_content("$10")
+  context "can CRUD Add Ons" do
+    it "can see Add On" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      expect(page).to have_content(add_on.name)
+    end
+
+    it "can create Add On" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      fill_in "Name", with: "Add On Name"
+      fill_in "Description", with: "Add On Description"
+      fill_in "Price", with: "10"
+      click_on "Create Add On Item"
+      wait_for_ajax_to_finish
+      expect(page).to have_content("Add On Name")
+      expect(page).to have_content("Add On Description")
+      expect(page).to have_content("$10")
+    end
+
+    it "can create Add On" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      fill_in "Name", with: "Add On Name"
+      fill_in "Description", with: "Add On Description"
+      fill_in "Price", with: "10"
+      click_on "Create Add On Item"
+      wait_for_ajax_to_finish
+      expect(page).to have_content("Add On Name")
+      expect(page).to have_content("Add On Description")
+      expect(page).to have_content("$10")
+    end
+
+    it "can update Add On" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      click_on "edit"
+      within('.panel-body') { fill_in "Name", with: "New Add On Name" }
+      within('.panel-body') { fill_in "Description", with: "New Add On Description" }
+      within('.panel-body') { fill_in "Price", with: "100" }
+      click_on "Update Add On"
+      wait_for_ajax_to_finish
+      expect(page).to have_content("New Add On Name")
+      expect(page).to have_content("New Add On Description")
+      expect(page).to have_content("$100")
+    end
+
+    it "can delete Add On" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      click_on "delete"
+      expect(page).to_not have_content(add_on.name)
+    end
+
+    it "can delete Add On" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      click_on "delete"
+      expect(page).to_not have_content(add_on.name)
+    end
   end
 
-  it "can update Add On" do
-    visit admin_dashboard_index_path
-    click_on "Add On Items"
-    click_on "edit"
-    within('.panel-body') { fill_in "Name", with: "New Add On Name" }
-    within('.panel-body') { fill_in "Description", with: "New Add On Description" }
-    within('.panel-body') { fill_in "Price", with: "100" }
-    click_on "Update Add On"
-    wait_for_ajax_to_finish
-    expect(page).to have_content("New Add On Name")
-    expect(page).to have_content("New Add On Description")
-    expect(page).to have_content("$100")
-  end
-
-  it "can delete Add On" do
-    visit admin_dashboard_index_path
-    click_on "Add On Items"
-    click_on "delete"
-    expect(page).to_not have_content(add_on.name)
+  context "#Active" do
+    it "can make an add on active" do
+      visit admin_dashboard_index_path
+      click_on "Add On Items"
+      click_on "active"
+      wait_for_ajax_to_finish
+      expect(add_on.reload).to be_active
+    end
   end
 end
