17d046818aa28bb3b7b1c554d6ac7ed516e0708a
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-06 16:27:09 -0700

    Render on-hold information in membership-hold-info component.

--- app/assets/javascripts/react/membership-hold-info.react.js
+++ app/assets/javascripts/react/membership-hold-info.react.js
@@ -5,8 +5,16 @@
 
 var MembershipHoldInfo = React.createClass({displayName: 'MembershipHoldInfo',
   render: function() {
+    var mem = this.props.membership;
     return (
-      React.DOM.div(null)
+      React.DOM.div(null,
+        React.DOM.h4(null, "Membership Hold Details"),
+        React.DOM.ul(null,
+          React.DOM.li({className: "list-group-item"}, "Membership hold starts: ", mem.hold_start),
+          React.DOM.li({className: "list-group-item"}, "There are ", mem.hold_weeks_remaining, " weeks remaining on your membership hold"),
+          React.DOM.li({className: "list-group-item"}, "Your membership hold ends on: ", mem.hold_end)
+        )
+      )
     );
   }
 });
--- app/assets/react/membership-hold-info.react.js
+++ app/assets/react/membership-hold-info.react.js
@@ -5,8 +5,16 @@
 
 var MembershipHoldInfo = React.createClass({
   render: function() {
+    var mem = this.props.membership;
     return (
-      <div />
+      <div>
+        <h4>Membership Hold Details</h4>
+        <ul>
+          <li className="list-group-item">Membership hold starts: {mem.hold_start}</li>
+          <li className="list-group-item">There are {mem.hold_weeks_remaining} weeks remaining on your membership hold</li>
+          <li className="list-group-item">Your membership hold ends on: {mem.hold_end}</li>
+        </ul>
+      </div>
     );
   }
 });
