dc177dccbf99839e7a14fd74018271e1b6aac8e8
Ed Shadi <edshadi@gmail.com>
2014-10-06 09:45:56 -0700

    subscription has a default_scope to price asc

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,7 @@
 class Subscription < ActiveRecord::Base
   validates :price, :meals, :weeks, :name, :presence => true
   validates :price, :meals, :weeks, :numericality => true
+  default_scope ->{order('price ASC')}
   has_many :memberships
   has_many :users, :through => :memberships
 end
