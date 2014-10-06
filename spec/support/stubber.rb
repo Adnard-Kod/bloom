03efa810f38f8e5d263615e2b29af6062bce0020
Steven Leu <steven.c.leu@gmail.com>
2014-10-06 14:08:20 -0700

    Add stub_current_admin_user helper for feature testing

--- spec/support/stubber.rb
+++ spec/support/stubber.rb
@@ -2,4 +2,9 @@ module Stubber
   def stub_current_user controller, user
     allow(controller).to receive(:current_user) { user }
   end
+  def stub_current_admin_user user
+    allow_any_instance_of(ApplicationController).to receive(:current_user) { user }
+    allow_any_instance_of(UserController).to receive(:current_user) { user }
+    allow_any_instance_of(AdminController).to receive(:current_user) { user }
+  end
 end
