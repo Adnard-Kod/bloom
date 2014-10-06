a1449a1a844593193415c29c531fafd8c014635d
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-06 13:10:43 -0700

    add Description to the test

--- spec/features/admin_menu_items_spec.rb
+++ spec/features/admin_menu_items_spec.rb
@@ -30,10 +30,11 @@ describe "Admin Menu Items", :js => true do
       click_on "Menu Items"
       click_on "edit"
       within('.panel-body') { fill_in "Name", with: "New Menu Item Name" }
-      within('.panel-body') { fill_in "Description", with: "New Menu Item Name" }
+      within('.panel-body') { fill_in "Description", with: "New Menu Item Description" }
       click_on "Update Menu Item"
       wait_for_ajax_to_finish
       expect(page).to have_content("New Menu Item Name")
+      expect(page).to have_content("New Menu Item Description")
     end
 
     it "can delete menu item" do
