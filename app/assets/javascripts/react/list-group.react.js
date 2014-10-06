8fdcda91f2571552a3b065ba79e960974c1b5b2a
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-06 16:20:23 -0700

    added class name to list group for testing.

--- app/assets/javascripts/react/list-group.react.js
+++ app/assets/javascripts/react/list-group.react.js
@@ -5,12 +5,13 @@
 //= require react/list-group-item.react
 var ListGroup = React.createClass({displayName: 'ListGroup',
   render: function() {
+    var categoryClass = this.props.categoryClass
     var items = [];
     this.props.list.forEach(function(item) {
       items.push(ListGroupItem({key: item.id, id: this.props.id, default: item.default, item: item.menu_item, admin: this.props.admin, user: this.props.user, selected: this.props.selected, quantity: item.quantity}))
     }.bind(this))
     return (
-      React.DOM.div({className: "list-group"},
+      React.DOM.div({className: "list-group", className: categoryClass},
         items
       )
     );
--- app/assets/react/list-group.react.js
+++ app/assets/react/list-group.react.js
@@ -5,12 +5,13 @@
 //= require react/list-group-item.react
 var ListGroup = React.createClass({
   render: function() {
+    var categoryClass = this.props.categoryClass
     var items = [];
     this.props.list.forEach(function(item) {
       items.push(<ListGroupItem key={item.id} id={this.props.id} default={item.default} item={item.menu_item} admin={this.props.admin} user={this.props.user} selected={this.props.selected} quantity={item.quantity} />)
     }.bind(this))
     return (
-      <div className="list-group">
+      <div className="list-group" className={categoryClass}>
         {items}
       </div>
     );
