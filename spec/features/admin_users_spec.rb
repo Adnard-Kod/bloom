8e8f325acbcbc68af3dc26c44023a5ad2072729f
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-06 13:12:47 -0700

    delete test for user profile

--- spec/features/admin_users_spec.rb
+++ spec/features/admin_users_spec.rb
@@ -17,24 +17,15 @@ describe "Admin Users", :js => true do
       click_on "delete"
       expect(page).to_not have_content(user.email)
     end
+
     it "Admin button makes user an admin" do
       non_admin = FactoryGirl.create :user
       visit admin_dashboard_index_path
       click_on "Users"
-      within('.panel-info') { click_on "Admin" }
+      within(".panel-info") { click_on "Admin" }
       wait_for_ajax_to_finish
       expect(non_admin.reload).to be_admin
     end
-    # it "view profile button show user profile" do
-    #   visit admin_dashboard_index_path
-    #   click_on "Users"
-    #   within(".panel-body") { click_on "view profile" }
-    #   # expect(page).to have_content("Create Address")
-    #   # find this button
-    # end
-  end
-  context "editing user profile form" do
-    #test this forms
   end
 
 end
