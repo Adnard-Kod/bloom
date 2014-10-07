cd017b634b714590829ba5fe20038f66b393ee9a
Gunnari Auvinen <gauvinen@gmail.com>
2014-10-06 17:24:48 -0700

    Add 'list-group' class to ul in membership-hold-info component.

--- app/assets/javascripts/react/membership-hold-info.react.js
+++ app/assets/javascripts/react/membership-hold-info.react.js
@@ -9,7 +9,7 @@ var MembershipHoldInfo = React.createClass({displayName: 'MembershipHoldInfo',
     return (
       React.DOM.div(null,
         React.DOM.h4(null, "Membership Hold Details"),
-        React.DOM.ul(null,
+        React.DOM.ul({className: "list-group"},
           React.DOM.li({className: "list-group-item"}, "Membership hold starts: ", mem.hold_start),
           React.DOM.li({className: "list-group-item"}, "There are ", mem.hold_weeks_remaining, " weeks remaining on your membership hold"),
           React.DOM.li({className: "list-group-item"}, "Your membership hold ends on: ", mem.hold_end)
--- app/assets/react/membership-hold-info.react.js
+++ app/assets/react/membership-hold-info.react.js
@@ -9,7 +9,7 @@ var MembershipHoldInfo = React.createClass({
     return (
       <div>
         <h4>Membership Hold Details</h4>
-        <ul>
+        <ul className="list-group">
           <li className="list-group-item">Membership hold starts: {mem.hold_start}</li>
           <li className="list-group-item">There are {mem.hold_weeks_remaining} weeks remaining on your membership hold</li>
           <li className="list-group-item">Your membership hold ends on: {mem.hold_end}</li>
