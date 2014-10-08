253b3a1c37fc92ac49a4cf016926d507e94fe6aa
Karma Hater! & Ernie Salazar <ehsalazar75@gmail.com>
2014-10-08 12:02:40 -0700

    downcase email before login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_email(params[:user][:email])
+    user = User.find_by_email(params[:user][:email].downcase)
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       render json: { redirect: user_dashboard_index_path }
