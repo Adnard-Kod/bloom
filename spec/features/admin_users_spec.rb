9a4428d747a520776ed128d77d5e09bd039b5c34
Ed Shadi <edshadi@gmail.com>
2014-10-07 18:21:06 -0700

    fix broken test due to link name change

--- spec/features/admin_users_spec.rb
+++ spec/features/admin_users_spec.rb
@@ -6,14 +6,14 @@ describe "Admin Users", :js => true do
   end
     it "can see all users" do
       visit admin_dashboard_index_path
-      click_on "Users"
+      click_on "User Management"
       expect(page).to have_content(user.email)
     end
 
   context "landing page for #admin-usrs" do
     it "delete button deletes a user" do
       visit admin_dashboard_index_path
-      click_on "Users"
+      click_on "User Management"
       click_on "delete"
       expect(page).to_not have_content(user.email)
     end
@@ -21,7 +21,7 @@ describe "Admin Users", :js => true do
     it "Admin button makes user an admin" do
       non_admin = FactoryGirl.create :user
       visit admin_dashboard_index_path
-      click_on "Users"
+      click_on "User Management"
       within(".panel-info") { click_on "Admin" }
       wait_for_ajax_to_finish
       expect(non_admin.reload).to be_admin
